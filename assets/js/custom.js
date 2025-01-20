3fe286e7b91e047e7aaab64f1905de0f26dc1685
didinsibua <didindong30@gmail.com>
2025-01-20 13:31:28 +0900

    add custom file

---
+++ assets/js/custom.js
@@ -0,0 +1,29 @@
+// Daftar teks yang akan diganti
+const texts = [
+    "Just a web developer making the web less buggy.",
+    "A lifelong learner.",
+    "Always improving through coding."
+  ];
+  
+  let currentIndex = 0;
+  const typingElement = document.getElementById('typing-text');
+  
+  function typeText() {
+    typingElement.innerHTML = ''; // Menghapus teks sebelumnya
+    const currentText = texts[currentIndex];
+    
+    let i = 0;
+    const interval = setInterval(() => {
+        typingElement.innerHTML += currentText[i];
+        i++;
+        if (i === currentText.length) {
+            clearInterval(interval);
+            setTimeout(() => {
+                currentIndex = (currentIndex + 1) % texts.length; // Berganti ke teks berikutnya
+                typeText(); // Memulai animasi lagi
+            }, 1000); // Tunggu 1 detik sebelum berganti teks
+        }
+    }, 100); // Kecepatan mengetik
+  }
+  
+  typeText();
